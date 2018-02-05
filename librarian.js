e63fcd5ffa2a002a8bda8c950d26ae1296770f15
Marufhasan74 <marufiub9@gmail.com>
2018-02-05 09:36:39 +0600

    shuvra

--- librarian.js
+++ librarian.js
@@ -3,23 +3,23 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
-  /* Task 2 */
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  { title: "Code complete", year: "1989", author: "Steve McConnell", publisher: "M. O. Company" }
 ];
 
 /* Task 1 */
 function getBookTitle( index ){
-  //add code
-}
+  return library[index].title;}
 
 console.log(getBookTitle(3));
 
 /* Task 3 */
 function addBook( book ){
-  //add code
+  library.push( book );
+  return library;
 }
-
-var newBook = {};
+//
+var newBook = { title: "Two hour of code", year: "1989", author: "Steve McConnell", publisher: "M. O. Company" };
 console.log(addBook(newBook));
 
 /* Task 4 */
