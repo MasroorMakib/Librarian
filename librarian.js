06d1dfc837e0ad7a025a3648c5d30386f00384e2
Shuvra Dev Saha <35439767+shuvradevsaha@users.noreply.github.com>
2018-02-12 23:48:48 +0600

    Assignment 5

--- librarian.js
+++ librarian.js
@@ -5,6 +5,7 @@ var library = [
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
   { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
   { title: "Code complete", year: "1989", author: "Steve McConnell", publisher: "M. O. Company" }
+   /* Task 2 */
 ];
 
 /* Task 1 */
@@ -24,14 +25,34 @@ console.log(addBook(newBook));
 
 /* Task 4 */
 function bookByAuthor(){
+	
   //add code
+    var n = library.length;
+  var temp;
+  for (var i = 0; i <= n-1; i++) {
+  	for (var j = 0; j <= n-1; j++) {
+  		if (library[i].author < library[j].author) {
+  			temp = library[i];
+  			library[i] = library[j];
+  			library[j] = temp;
+  		}
+  	}
+  }
+return library;
 }
 
 console.log(bookByAuthor());
 
 /* Task 5 */
 function findByTitle( title ){
-  //add code
+  var pat= new RegExp(title);
+  for (var i = 0; i < library.length; i++) {
+    if (library[i].title.match(pat)) {
+
+      return library[i].title;
+
+    }
+  }
 }
 
 console.log(findByTitle("man"));
