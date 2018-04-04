b7793f87dc49507e6e9018dc010cbd2240169e19
MasroorMakib <35513745+MasroorMakib@users.noreply.github.com>
2018-04-04 08:44:16 +0600

    Add files via upload

--- librarian.js
+++ librarian.js
@@ -3,7 +3,7 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf " },
   { title: "Code complete", year: "1989", author: "Steve McConnell", publisher: "M. O. Company" }
    /* Task 2 */
 ];
@@ -25,18 +25,18 @@ console.log(addBook(newBook));
 
 /* Task 4 */
 function bookByAuthor(){
-	
+  
   //add code
     var n = library.length;
   var temp;
   for (var i = 0; i <= n-1; i++) {
-  	for (var j = 0; j <= n-1; j++) {
-  		if (library[i].author < library[j].author) {
-  			temp = library[i];
-  			library[i] = library[j];
-  			library[j] = temp;
-  		}
-  	}
+    for (var j = 0; j <= n-1; j++) {
+      if (library[i].author < library[j].author) {
+        temp = library[i];
+        library[i] = library[j];
+        library[j] = temp;
+      }
+    }
   }
 return library;
 }
